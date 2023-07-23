8353f8a85259ab00fa77f8c3fa1f5c41590a66ae
Luis Antonio Orbegoso Moreno <antoniorbegosomoreno@gmail.com>
2023-07-23 16:37:27 -0500

    archivo con la clase alumnos en python

---
+++ alumnos.py
@@ -0,0 +1,12 @@
+class alumnos:
+    def __init__(self,nombre,apellidos,edad,nacionalidad):
+        self.nombre = nombre
+        self.apellidos =apellidos
+        self.edad = edad
+        self.nacionalidad = nacionalidad
+        self.nota = 0
+    def leerNota(self):
+        return self.nota
+    def registrarNota(self,notaAlumno):
+        self.nota = notaAlumno
+    
